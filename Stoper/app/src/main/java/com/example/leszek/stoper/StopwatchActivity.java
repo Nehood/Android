e316827117a1b4192de7fb77932bbd9e09f9c73f
Nehood <gorkiewiczm@gmail.com>
2016-07-26 19:04:01 +0200

    stopwatch - handler

---
+++ Stoper/app/src/main/java/com/example/leszek/stoper/StopwatchActivity.java
@@ -0,0 +1,54 @@
+package com.example.leszek.stoper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+public class StopwatchActivity extends Activity {
+
+    private int seconds;
+    private boolean isRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    public void onClickStart(View view)
+    {
+        isRunning = true;
+    }
+
+    public void onClickStop(View view)
+    {
+        isRunning = false;
+    }
+
+    public void onClickReset(View view)
+    {
+        isRunning = false;
+        seconds = 0;
+    }
+
+    private void runTimer()
+    {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds / 3600) % 60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (isRunning) seconds++;
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
